804fb21311f0cae3a561f059c36ba4e5983f2fd8
chetran <chelongtran@gmail.com>
2026-02-25 18:13:57 +0100

    Contents in the player box, now aligns better, and added an edit button for future actions

--- app/components/PlayerBox.tsx
+++ app/components/PlayerBox.tsx
@@ -1,14 +1,32 @@
 export default function PlayerBox(prop: { player: string; removePlayer: (name: string) => void }) {
+    var name = prop.player.trim().replace(/[\n\r\t]/gm, "-");
+    var maxLength = 10;
+    if (prop.player.length > maxLength) {
+        name = prop.player.substring(0, maxLength - 3) + "...";
+    }
+
     return (
-        <div className="border border-gray-300 rounded p-4 mb-4">
-            <h3 className="text-lg font-semibold inline m-4 mr-32">{prop.player}</h3>
-    
+        <div className="container border border-gray-400 rounded p-4 mb-4 grid grid-cols-4">
+            <div className="col-span-2">
+                <h3 className="text-lg font-semibold m-4 mr-32">{name}</h3>
+            </div>
+
+            <div className="text-sm justify-end m-auto">
                 <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                >
+                Edit
+                </button>
+            </div>
+            <div className="text-sm justify-end m-auto">
+                    <button
                     onClick={() => prop.removePlayer(prop.player)}
                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
-                >
-                    Remove
-                </button>
+                    >
+                    Delete
+                    </button>
+                </div>
+            
         </div>
     );
 }         
